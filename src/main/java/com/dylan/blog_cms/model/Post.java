4b53d04ad3e7bbcc173a05cf8ba9e5bf93e3fa00
nom nom <detu124@gmail.com>
2026-07-10 09:48:45 +0700

    Initial commit

---
+++ src/main/java/com/dylan/blog_cms/model/Post.java
@@ -0,0 +1,62 @@
+package com.dylan.blog_cms.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "posts")
+public class Post {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    @Column(columnDefinition = "TEXT")
+    private String content;
+    private String category;
+    private boolean published;
+
+    public Post() {
+    }
+
+    public Post(String title, String content, String category) {
+        this.title = title;
+        this.content = content;
+        this.category = category;
+        this.published = false;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public boolean isPublished() {
+        return published;
+    }
+
+    public void setPublished(boolean published) {
+        this.published = published;
+    }
+}
